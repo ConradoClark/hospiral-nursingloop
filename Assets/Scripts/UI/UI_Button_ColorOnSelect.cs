db421b7b2fa60e8bfa98b2c7007eb4100179a5d9
ConradoClarke <conradoclarke@gmail.com>
2023-03-19 17:34:26 -0300

    Main Menu / Credits / Level Select

---
+++ Assets/Scripts/UI/UI_Button_ColorOnSelect.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Licht.Unity.Objects;
+using Licht.Unity.UI;
+using UnityEngine;
+
+public class UI_Button_ColorOnSelect : BaseGameObject
+{
+    [field: SerializeField]
+    public UIAction UIAction { get; private set; }
+
+    [field: SerializeField]
+    public SpriteRenderer SpriteRenderer { get; private set; }
+
+    [field: SerializeField]
+    public Color DefaultColor { get; private set; }
+
+    [field: SerializeField]
+    public Color SelectedColor { get; private set; }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        UIAction.OnSelected += UIAction_OnSelected;
+        UIAction.OnDeselected += UIAction_OnDeselected;
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        UIAction.OnSelected -= UIAction_OnSelected;
+        UIAction.OnDeselected -= UIAction_OnDeselected;
+    }
+
+    private void UIAction_OnDeselected()
+    {
+        SpriteRenderer.color = DefaultColor;
+    }
+
+    private void UIAction_OnSelected(bool obj)
+    {
+        SpriteRenderer.color = SelectedColor;
+    }
+}
